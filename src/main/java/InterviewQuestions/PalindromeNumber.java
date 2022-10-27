b2892336a4b4706dc8e27fe5477406c60fd1abbf
vikum1407 <vikum1407@gmail.com>
2022-10-27 18:05:30 +0530

    added palidromeNumber

---
+++ src/main/java/InterviewQuestions/PalindromeNumber.java
@@ -0,0 +1,32 @@
+package InterviewQuestions;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+
+        //Get the user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number Series: ");
+
+        //Get the user enteres number into the 'num' variable
+        int num=sc.nextInt();
+
+        int originalNum=num;
+
+        //Reverse the number method
+        int rev=0;
+        while (num!=0)
+        {
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        if (originalNum==rev)
+        {
+            System.out.println(originalNum +" Palindrome Number");
+        }else
+        {
+            System.out.println(originalNum +" Not a Palindrome Number");
+        }
+    }
+}
